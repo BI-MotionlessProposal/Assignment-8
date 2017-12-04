f5e5c7c20929bcad6479b0fc60c85638277f6181
Emmely Lundberg <Emmely Lundberg>
2017-12-04 14:19:33 +0100

    first commit

---
+++ assignment8.py
@@ -0,0 +1,35 @@
+# Part 1: Accuracy and recall
+
+# Part 1.1 
+# Accuracy refers to the closeness of a measured value to a standard or known value. 
+# Precision refers to the closeness of two or more measurements to each other. 
+# Precision is independent of accuracy. You can be very precise but inaccurate, as described above. You can also be accurate but imprecise. (reference: https://labwrite.ncsu.edu/Experimental%20Design/accuracyprecision.htm)
+
+# Part 1.2
+# Precision (also called positive predictive value) is the fraction of relevant instances among the retrieved instances.
+# Recall is the fraction of relevant instances that have been retrieved over the total amount of relevant instances. (reference https://en.wikipedia.org/wiki/Precision_and_recall)
+
+# Part 1.3
+
+# Using sklearn.metrics.classification_report print the result from your breast cancer classification from last week. Explain what is going on in at least two lines of text.
+
+# Part 2: Population and t-test
+
+# Part 2.1
+
+
+# Part 2.2
+
+
+# Part 2.3
+
+
+# Part 3 (OPTIONAL): Training a perceptron network
+
+
+# Part 3.1
+
+
+# Part 3.2
+
+
